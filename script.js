eb1c2d2f01bb552fd5d59e1f8082d4ce17b97e05
wheatwhole <117047051+wheatwhole@users.noreply.github.com>
2023-05-16 11:08:28 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function fix()
+{
+    var el = this;
+    var par = el.parentNode;
+    var next = el.nextSibling;
+    par.removeChild(el);
+    setTimeout(function() {par.insertBefore(el, next);}, 0)
+}
+
