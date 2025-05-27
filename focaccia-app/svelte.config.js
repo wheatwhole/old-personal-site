b37ed01eebcd6c576849bcb0b1c07df6ec3bf877
wheatwhole <117047051+wheatwhole@users.noreply.github.com>
2025-05-27 15:12:06 -0400

    last commit before switching

--- focaccia-app/svelte.config.js
+++ focaccia-app/svelte.config.js
@@ -11,18 +11,21 @@ const config = {
     // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
     // See https://svelte.dev/docs/kit/adapters for more information about adapters.
-    adapter: adapter({
-      fallback: "app.html",
-    }),
+    	adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		}),
     alias: {
       // an alias ending /* will only match
       // the contents of a directory, not the directory itself
       "$components/*": "./src/components/*",
       "$static/*": "./static/*",
-    },
-    paths: {
-      base: "/wheatwhole.github.io/focaccia-app",
-    },
+    }
   },
 };
 
